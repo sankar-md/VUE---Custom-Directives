f349242e2ea2ffb4b897972b09c5a24bffa37872
sankar-md <sankarsmd100@gmail.com>
2025-04-23 08:29:35 +0530

    some added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div v-size>Hello World!</div>
+  <div v-size v-highlight>Hello World!</div>
 </template>
 
 <script>
+const highlight = {
+  mounted: (el) => el.classList.add("is-highlight"),
+};
 export default {
   name: "App",
   components: {},
+  directives: {
+    highlight,
+  },
 };
 </script>
 
@@ -17,5 +23,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.is-highlight {
+  background-color: orange;
+  color: red;
+  padding: 10px;
+  border-radius: 5px;
 }
 </style>
